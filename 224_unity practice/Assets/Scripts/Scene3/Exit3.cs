1f6b4a2b6de7c3fbc73802096f9cf32c47d5cd4c
Scone111 <Xu18801148788@163.com>
2024-10-03 20:49:26 +0800

    10-3Animate

---
+++ 224_unity practice/Assets/Scripts/Scene3/Exit3.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit3 : MonoBehaviour
+{
+    public Animator anim;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            anim.SetTrigger("Bed");
+        }
+    }
+}
